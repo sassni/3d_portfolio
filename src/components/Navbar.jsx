512acb1cd11b6abe16a51b07733fa5b4105abaf1
Sasni MRM <sasni.20200168@iit.ac.lk>
2023-06-22 20:36:26 +0530

    Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,14 @@ import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
-  const [active, setActive] = userState("");
+  const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
-  const [scrollTo, setScrolled] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      const scrollTo = window.scrollY;
-      if (scrollTo > 100) {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
         setScrolled(true);
       } else {
         setScrolled(false);
@@ -28,8 +28,8 @@ const Navbar = () => {
   return (
     <nav
       className={`${
-        style.paddindX
-      } w-full flex item-center py-5 fixed top-0 z-20 ${
+        styles.paddingX
+      } w-full flex items-center py-5 fixed top-0 z-20 ${
         scrolled ? "bg-primary" : "bg-transparent" }`}>
 
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
@@ -37,8 +37,25 @@ const Navbar = () => {
           setActive("");
           window.scrollTo(0, 0);
         }}>
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
+            Sasni &nbsp;
+            <span className='sm:block hidden'> | Web Developer</span>
+          </p>
         </Link>
+
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((nav) => (
+            <li
+            key={nav.id}
+            className={`${
+              active === nav.title ? "text-white" : "text-secondary"
+            } hover:text-white text-[18px] font-medium cursor-pointer`}
+            onClick={() => setActive(nav.title)}>
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+          </ul>
       </div>
     </nav>
   )
